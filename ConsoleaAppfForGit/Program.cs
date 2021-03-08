041c27bccf3285c85d79918b8eaa40d64b00576a
Rozenbergas <63879482+Rozenbergas@users.noreply.github.com>
2021-03-08 18:44:57 +0200

    Add project files.

---
+++ ConsoleaAppfForGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleaAppfForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
